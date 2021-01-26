a4e2e129eb685fb8a939990d226d1d07f9e8e0c4
dilan weerasinghe <dilanweerasinghe97@outlook.com>
2021-01-26 15:07:45 +0530

    second commit menu change

--- machine.py
+++ machine.py
@@ -2,7 +2,7 @@ import cProfile
 
 
 class coffee:
-    def __init__(self,name, price, orgin, mix, stock):
+    def __init__(self, name, price, orgin, mix, stock):
         self.name = name
         self.price = price
         self.origin = orgin
@@ -11,38 +11,44 @@ class coffee:
 
     def add_new_coffee(self):
 
-            coffeeditails.append([self.name, self.price, self.orgin, self.mix, self.stock])
-            print("New coffee type added successfully")
+        coffeeditails.append(
+            [self.name, self.price, self.orgin, self.mix, self.stock])
+        print("New coffee type added successfully")
 
 
-coffeeditails = [["Latte", 250, "USA", "Milkkmix", 20],
+coffeeditails = [["Latte", 250, "USA", "Milkkmix", 0],
                  ["Black Coffee", 200, "LK", "Blackcoffeemix", 10]]
+
 
 def display_coffees():
     print("ID| CoffeeName | Price | Origin | Mix packets Type | Packets Quantity")
 
-    for i in range(len(coffeeditails )):
-        print(i, "|", coffeeditails [i][0], "|", coffeeditails [i][1], "|", coffeeditails [i][2], "|", coffeeditails [i][3], "|",
-              coffeeditails [i][4])
+    for i in range(len(coffeeditails)):
+        print(i, "|", coffeeditails[i][0], "|", coffeeditails[i][1], "|", coffeeditails[i][2], "|", coffeeditails[i][3], "|",
+              coffeeditails[i][4])
+
 
 def siz_coffee_index():
     return len(coffeeditails)
 
-#cProfile.run('siz_coffee_index()')
+# cProfile.run('siz_coffee_index()')
+
 
 def buy_coffee():
     try:
         show_Coffee_Details()
-        userinput= int(input("Enter the coffee id :"))
+        userinput = int(input("Enter the coffee id :"))
         if userinput < siz_coffee_index():
             coines = int(input('Enter the coins: '))
-            if coines > coffeeditails [userinput][1]:
-                coffeeditails [userinput][4] = coffeeditails [userinput][4] - 1
+            if coines > coffeeditails[userinput][1]:
+                coffeeditails[userinput][4] = coffeeditails[userinput][4] - 1
 
-                print("Take your remain {}".format(coines - coffeeditails [userinput][1]))
+                print("Take your remain {}".format(
+                    coines - coffeeditails[userinput][1]))
                 print("Enjoy !!!")
             else:
                 print("Not enough money enter more coins:")
+
         else:
             print("Coffee id is dose not exit")
             buy_coffee()
@@ -55,11 +61,12 @@ def buy_coffee():
 def show_Coffee_Details():
     print("ID| Coffee Name | Price | Origin")
 
-    for i in range(len(coffeeditails )):
-        print(i,"|", coffeeditails [i][0],"|",
-              coffeeditails [i][1],"|", coffeeditails [i][2])
+    for i in range(len(coffeeditails)):
+        print(i, "|", coffeeditails[i][0], "|",
+              coffeeditails[i][1], "|", coffeeditails[i][2])
 #
 # cProfile.run('show_Coffee_Details()')
+
 
 def menu():
     print("\nWELCOME TO AUTOMATIC COFFEE MAKER  ")
@@ -70,13 +77,13 @@ def menu():
     print("____________________________")
 
     try:
-        coine = int(input("Enter your step :"))
+        step = int(input("Enter your step :"))
 
-        if coine == 1:
+        if step == 1:
             buy_coffee()
-        elif coine == 2:
+        elif step == 2:
             show_Coffee_Details()
-        elif coine == 3:
+        elif step == 3:
             exit()
         else:
             print("Enter valid number")
@@ -87,4 +94,3 @@ def menu():
 
 
 menu()
-
